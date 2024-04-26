74e42374755f659b942ce299aa8dff378bfade9e
Cristhian_Guilherme <111366428+CRISTHIANwew@users.noreply.github.com>
2024-04-26 10:38:24 -0300

    1

---
+++ Principal.pas
@@ -0,0 +1,53 @@
+unit Principal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView.Types,
+  FMX.ListView.Appearances, FMX.ListView.Adapters.Base, FMX.ListView,
+  FMX.Layouts, FMX.ListBox;
+
+type
+  TForm1 = class(TForm)
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Circle1: TCircle;
+    Circle2: TCircle;
+    Rectangle3: TRectangle;
+    Label1: TLabel;
+    Rectangle4: TRectangle;
+    Label2: TLabel;
+    Rectangle5: TRectangle;
+    Label3: TLabel;
+    Circle3: TCircle;
+    Circle4: TCircle;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Label4: TLabel;
+    Rectangle8: TRectangle;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Rectangle9: TRectangle;
+    Rectangle10: TRectangle;
+    Label10: TLabel;
+    Rectangle11: TRectangle;
+    Rectangle12: TRectangle;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
